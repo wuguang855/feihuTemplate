34cc160542251b22e59ee3e6f4b80bbf2afc2a0a
EDZ <16953955@qq.com>
2020-10-22 13:48:44 +0800

    给模板添加动态信息

--- src/router.js
+++ src/router.js
@@ -1,17 +1,21 @@
-import React, { lazy } from 'react';
-import { Router, Route, Switch, IndexRoute } from 'dva/router';
-import withSuspense from './components/withSuspense';
+import React from "react";
+import { Router, Route, Switch, IndexRoute } from "dva/router";
+import dynamic from "dva/dynamic";
 
-//同步加载
-import IndexPage from './pages/Index';
-import P1 from './pages/P1';
-import P2 from './pages/P2';
-//按需加载
-// const IndexPage = withSuspense(lazy(() => import('./pages/Index')));
-// const P1 = withSuspense(lazy(() => import('./pages/P1')));
-// const P2 = withSuspense(lazy(() => import('./pages/P2')));
 
-function RouterConfig({ history }) {
+function RouterConfig({ history, app }) {
+  //同步加载
+  import IndexPage from "./pages/Index";
+  import P1 from "./pages/P1";
+  //按需加载
+  const P2 = dynamic({
+    app,
+    models: () => [
+      //按需加载models
+      //import('./models/some modle'),
+    ],
+    component: () => import("./pages/P2"),
+  });
 
   return (
     <Router history={history}>

1527be681ca3943736db9d4dfc70b13f488e385e
EDZ <16953955@qq.com>
2020-10-23 10:15:27 +0800

    feat:添加i8in示例

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,39 +1,38 @@
 import React, { Component } from 'react';
-import { connect } from 'dva';
+import { withTranslation } from 'react-i18next';
 import { List, Row, Col, Button } from 'tea-component';
 
 import Title from './Title';
 import styles from './index.less';
 
+@withTranslation()
 export default class IndexPage extends Component {
   static defaultProps = {};
   constructor(props) {
     super(props);
-    this.state ={
-
-    }
+    this.state = {};
   }
   render() {
-    console.log(this.props);
+    const { t } = this.props;
     const style = { marginRight: 5 };
     return (
       <Row>
         <Col>
-          <Title>TEA-UI 演示</Title>
+          <Title>{t('TEA-UI演示')}</Title>
           <List split="stripe">
-            <List.Item className={styles.line}> 按钮 </List.Item>
+            <List.Item className={styles.line}> {t('按钮')} </List.Item>
             <List.Item className={styles.line}>
               <Button style={style} type="primary">
-                主要按钮
+                {t('主要按钮')}
               </Button>
               <Button loading style={style} type="primary">
-                主要按钮
+                {t('主要按钮')}
               </Button>
               <Button disabled style={style} type="primary">
-                禁用
+                {t('禁用')}
               </Button>
               <Button style={style} type="pay">
-                支付按钮
+                {t('支付按钮')}
               </Button>
             </List.Item>
           </List>
